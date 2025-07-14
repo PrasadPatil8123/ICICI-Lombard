a1964b4d3a78d7fd5ed1d148be899a3a5bd7ac99
PrasdPatil8123 <prasadpatil2003128@gmail.com>
2025-07-14 20:22:48 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { PipesPipe } from './shared/pipes/pipes.pipe';
+import { DirectivesDirective } from './shared/directives/directives.directive';
+import { HeaderComponent } from './common/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipesPipe,
+    DirectivesDirective,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
