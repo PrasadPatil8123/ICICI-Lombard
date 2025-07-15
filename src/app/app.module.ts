bb683d2ef7470c619f6979fb5983d1588fedfdfe
ghulenilesh123 <ghulenilesh34@gmail.com>
2025-07-14 20:34:33 -0700

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './all footer/footer/footer.component';
 import { Footer2Component } from './all footer/footer2/footer2.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    Footer2Component
+    Footer2Component,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
