c44229fb883544b17a72844e5453777b50ed4293
ghulenilesh123 <ghulenilesh34@gmail.com>
2025-07-23 15:16:52 -0700

    add data

--- src/app/common/all footer/footer/footer.component.ts
+++ src/app/common/all footer/footer/footer.component.ts
@@ -50,7 +50,8 @@ footerData1 = [
     }
     
   ]
-  footerData3=[ {
+  footerData3=[ 
+    {
       title: 'Legal',
       links: [
         'Privacy Policy', 'Insure App Privacy Policy', 'Products Withdrawn', 'Do Not Call Registry',
@@ -83,5 +84,85 @@ footerData1 = [
         'Pradhan Mantri Suraksha Bima Yojana', 'Crop Insurance - Login',
         'Hospital Empanelment criteria', 'Account Aggregator', 'International Business (IIO)'
       ]
-    },]
+    },
+  ]
+
+
+    // ---------------------
+    activeIndex: number | null = null;
+
+toggleSection(index: number): void {
+  this.activeIndex = this.activeIndex === index ? null : index;
+}
+
+
+
+footerData = [
+  {
+    title: 'Products',
+    links: [ 'Motor Insurance', 'Car Insurance', 'Two Wheeler Insurance', 'Health Insurance',
+        'Health AdvantEdge', 'Elevate', 'MaxProtect', 'Activate Booster (Combo*)',
+        'Activate Booster', 'Personal Accident Insurance', 'Arogya Sanjeevani Policy, ICICI Lombard',
+        'Travel Insurance', 'Single Trip Travel', 'Multi Trip Travel',]
+  },
+  {
+    title: 'Services',
+    links: ['Customer Support', 'Citizen Charter', 'Grievance Redressal', 'Retrieve Quote',
+        'Unclaimed Amount', 'Intimate PA claim', 'Track Claim', 'Marine/Property Claims',
+        'Live Chat', 'Renew Your Policy', 'Portability', 'Whatsapp Notification',]
+  },
+   {
+    title: 'About Us',
+    links: ['Overview', 'Promoters', 'CSR', 'Risk Management', 'Public Disclosures']
+  },
+
+  {
+      title: 'Info Centre',
+      links: ['Expert Blog', 'Insurance Information', 'Infocentre', 'FAQ']
+    },
+  {
+      title: 'Data on Health Claim Service Indicators',
+      links: ['April 2025', 'May 2025', 'June 2025']
+    },
+
+   {
+      title: 'Legal',
+      links: [
+        'Privacy Policy', 'Insure App Privacy Policy', 'Products Withdrawn', 'Do Not Call Registry',
+        'General Terms & Conditions', 'Disclaimer', 'Insurance Ombudsman', 'Stewardship Policy',
+        'Disclosure under Stewardship Policy', "Policy for Policyholder’s Interest Protection & Grievance Redressal",
+        'Advisory to Customer and Channel Partners', 'ICICI Lombard Product List',
+        'GRO Details of Active Branches', 'Motor Third Party claims - Statewise nodal officer details',
+        'Whistle Blower Policy'
+      ]
+    }, 
+
+   {
+      title: 'Others',
+      links: [
+        'Agents Portal', 'Corporate Login', 'Careers', 'Online Manager', 'Suspended Agents',
+        'BAGI Blacklisted Agents', 'Distribution Channels', 'Sitemap',
+        'Pradhan Mantri Suraksha Bima Yojana', 'Crop Insurance - Login',
+        'Hospital Empanelment criteria', 'Account Aggregator', 'International Business (IIO)'
+      ]
+    },
+     {
+    title: 'PMFBY',
+    links: [
+      'Pradhan Mantri Fasal',
+      'Bima Yojana',
+      'Toll Free No: 1800 266 9725',
+      '(Available 9 Am To 9 Pm)'
+    ]
+  },
+
+ {
+      title: 'IL TakeCare app',
+      images: [
+        { src: '../assets/img/playstore.png', alt: 'Google Play' },
+      ]
+    }
+];
+
+
 }
