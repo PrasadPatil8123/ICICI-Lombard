5267e817e0d3bd7ea01301c46ad8f6f1cb811cbf
Priya Chemate <chematepriyagit config user.namegit config user.email chematepriya>
2025-07-24 09:04:21 -0700

    Adding navbar code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
+
 const routes: Routes = [
+  {path : 'motor', loadChildren: ()=> import('./features/motor-insurance/motor-insurance.module')
+    .then (mod => mod.MotorInsuranceModule)
+  },
   {path : 'claim', loadChildren:() => import('./features/claims/claims.module')
     .then(mod => mod.ClaimsModule)
+  },
+  {path : 'corporate-inc', loadChildren:() => import('./features/corporate-insurance/corporate-insurance.module')
+    .then(mod => mod.CorporateInsuranceModule)
+  },
+  {path : 'health-inc', loadChildren:() => import('./features/health-insurance/health-insurance.module')
+    .then(mod => mod.HealthInsuranceModule)
+  },
+  {path : 'other-inc', loadChildren:()=> import('./features/other-insurance/other-insurance.module')
+    .then(mod => mod.OtherInsuranceModule)
+  },
+  {path : 'renewal-inc', loadChildren:()=> import('./features/renewals/renewals.module')
+    .then(mod => mod.RenewalsModule)
+  },
+  {path : 'sme-inc', loadChildren:()=> import('./features/sme-insurance/sme-insurance.module')
+    .then(mod => mod.SmeInsuranceModule)
+  },
+  {path : 'travel-inc', loadChildren:()=> import('./features/travel-insurance/travel-insurance.module')
+    .then(mod => mod.TravelInsuranceModule)
   }
+
 ]
 
 @NgModule({
