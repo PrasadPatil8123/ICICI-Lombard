3edb4ce2b47037e4bae25257f11768515160149b
sunilkatare <sunil.kataresk@gmail>
2025-07-25 12:00:18 +0530

    corporate claim change

--- src/app/features/claims/corp-claims/corp-claims.component.ts
+++ src/app/features/claims/corp-claims/corp-claims.component.ts
@@ -15,6 +15,18 @@ export class CorpClaimsComponent {
   public email: any = 'customersupport@icicilombard.com';
   public listOfData: any = [];
   public countId: number = 1;
+
+  public trackListData: any = [];
+  public trackCountId: number = 1;
+
+  intimateClaim: FormGroup;
+  public intimateClaimDataList: any = []
+  public intimateClaimCountId: number = 1;
+
+  trackClaim: FormGroup;
+  public trackClaimsListData: any = []
+  public trackClaimsCountId: number = 1;
+
   // policyNumber: any;
   constructor(private fb: FormBuilder) {
     this.intimateForm = this.fb.group({
@@ -26,9 +38,21 @@ export class CorpClaimsComponent {
     });
 
     this.trackForm = this.fb.group({
+      id: [''],
       claimRef: ['', Validators.required],
       loanNumber: ['', Validators.required]
     });
+
+    this.intimateClaim = fb.group({
+      id: [''],
+      policyNumbe: [''],
+      policyStartDate: ['']
+    })
+    this.trackClaim = fb.group({
+      id: [''],
+      claimRefNumber: [''],
+      lossDate: ['']
+    })
   }
 
   onIntimateSubmit() {
@@ -40,9 +64,24 @@ export class CorpClaimsComponent {
   }
 
   onTrackSubmit() {
+    this.trackForm.value.id = this.trackCountId++;
+    this.trackListData.push({ ...this.trackForm.value });
     if (this.trackForm.valid) {
       console.log("Track Claim Data:", this.trackForm.value);
     }
+  }
+
+  public intimateClaimSubmit() {
+    this.intimateClaim.value.id = this.intimateClaimCountId++;
+    this.intimateClaimDataList.push({ ...this.intimateClaim });
+    console.log(this.intimateClaimDataList);
+
+  }
+  public trackClaimSubmit() {
+    this.trackClaim.value.id = this.trackClaimsCountId++;
+    this.trackClaimsListData.push({ ...this.trackClaim })
+    console.log(this.trackClaimsListData);
+
   }
 
   get f() {
