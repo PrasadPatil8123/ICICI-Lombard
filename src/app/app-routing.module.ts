0e933dbcd788ab95e52065d84351d286944d687a
ghulenilesh123 <ghulenilesh34@gmail.com>
2025-07-25 23:16:27 -0700

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: 'claims', loadChildren: () => import('./features/claims/claims.module')
       .then(mod => mod.ClaimsModule)
+  },
+   {
+    path: 'rsk', loadChildren: () => import('../app/features/corporate-insurance/corporate-insurance.module')
+      .then(mod => mod.CorporateInsuranceModule)
   }
 ]
 
--- src/app/features/corporate-insurance/corporate-insurance.module.ts
+++ src/app/features/corporate-insurance/corporate-insurance.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RiskManagementComponent } from './risk-management/risk-management.component';
+import { Routes } from '@angular/router';
 
-
+ let route :Routes=[
+   {path:"rskk",component:RiskManagementComponent}
+ ]
 
 
 @NgModule({
